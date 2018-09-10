102a3ae27639732ab6368bf38bef2a0351437dbe
Rubal Walia <rubal.walia@dotsquares.com>
2018-09-10 13:40:22 +0530

    Staging

---
+++ SmsBuddy/SmsBuddy/Models/TemplateModel.cs
@@ -0,0 +1,19 @@
+﻿using NullVoidCreations.WpfHelpers.Base;
+
+namespace SmsBuddy.Models
+{
+    class TemplateModel: NotificationBase
+    {
+        string _name;
+
+        #region properties
+
+        public string Name
+        {
+            get { return _name; }
+            set { Set(nameof(Name), ref _name, value); }
+        }
+
+        #endregion
+    }
+}
