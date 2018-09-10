3f18f1bc1823983e68bfd4c546a1c6b569bda030
Rubal Walia <walia.rubal@gmail.com>
2018-09-10 21:04:43 +0530

    Staging

--- SmsBuddy/SmsBuddy/Models/TemplateModel.cs
+++ SmsBuddy/SmsBuddy/Models/TemplateModel.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 
 namespace SmsBuddy.Models
 {
-    [TableName(nameof(TemplateModel)]
+    [TableName(nameof(TemplateModel))]
     [PrimaryKey(nameof(Id))]
     class TemplateModel: NotificationBase
     {
