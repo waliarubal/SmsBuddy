b574cfbe148978bbebc93a9240a634cde28782e2
Rubal Walia <walia.rubal@gmail.com>
2018-09-12 08:15:43 +0530

    Initial commit.

---
+++ SmsBuddy/SmsBuddy/Gateway/GatewayBase.cs
@@ -0,0 +1,49 @@
+﻿using NullVoidCreations.WpfHelpers.Base;
+using NullVoidCreations.WpfHelpers.DataStructures;
+using System.Collections.Generic;
+
+namespace SmsBuddy.Gateway
+{
+    abstract class GatewayBase: NotificationBase
+    {
+        string _name;
+        IEnumerable<Doublet<string, object>> _parameters;
+
+        #region constructor/destructor
+
+        protected GatewayBase(string name)
+        {
+            Name = name;
+        }
+
+        #endregion
+
+        #region properties
+
+        public string Name
+        {
+            get { return _name; }
+            private set { Set(nameof(Name), ref _name, value); }
+        }
+
+        public IEnumerable<Doublet<string, object>> Parameters
+        {
+            get { return _parameters; }
+            private set { Set(nameof(Parameters), ref _parameters, value); }
+        }
+
+        #endregion
+
+        protected void SetParameterNames(IEnumerable<string> parameterNames)
+        {
+            var parameters = new List<Doublet<string, object>>();
+            foreach (var parameterName in parameterNames)
+                parameters.Add(new Doublet<string, object>(parameterName, null));
+            Parameters = parameters;
+        }
+
+        public abstract bool SendSms(string text);
+
+        public abstract long GetBalance();
+    }
+}
---
+++ SmsBuddy/SmsBuddy/Gateway/Textlocal.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SmsBuddy.Gateway
+{
+    class Textlocal : GatewayBase
+    {
+        public Textlocal(): base("Textlocal")
+        {
+            var parameterNames = new List<string> { "Dummy" };
+            SetParameterNames(parameterNames);
+        }
+
+        public override long GetBalance()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool SendSms(string text)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
