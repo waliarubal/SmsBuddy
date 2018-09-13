7956cb1f49e4fe58dae700c69d5d6ede35b7d5bb
Rubal Walia <rubal.walia@dotsquares.com>
2018-09-13 11:54:36 +0530

    Staging

---
+++ SmsBuddy/SmsBuddy/ViewModels/MainViewModel.cs
@@ -0,0 +1,10 @@
+﻿using NullVoidCreations.WpfHelpers.Base;
+using System.Windows.Input;
+
+namespace SmsBuddy.ViewModels
+{
+    class MainViewModel: ViewModelBase
+    {
+        ICommand _showView;
+    }
+}
