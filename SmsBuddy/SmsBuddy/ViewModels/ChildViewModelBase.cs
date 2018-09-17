368851122b3c499dd41cc86ce1c6e9afeb3df052
Rubal Walia <walia.rubal@gmail.com>
2018-09-17 09:09:01 +0530

    New code base.

---
+++ SmsBuddy/SmsBuddy/ViewModels/ChildViewModelBase.cs
@@ -0,0 +1,31 @@
+﻿using NullVoidCreations.WpfHelpers.Base;
+
+namespace SmsBuddy.ViewModels
+{
+    abstract class ChildViewModelBase: ViewModelBase
+    {
+        string _title, _icon;
+        bool _isInitialized;
+
+        protected ChildViewModelBase(string title)
+        {
+            Title = title;
+        }
+
+        #region properties
+
+        public string Title
+        {
+            get { return _title; }
+            private set { Set(nameof(Title), ref _title, value); }
+        }
+
+        public string Icon
+        {
+            get { return _icon; }
+            protected set { Set(nameof(Icon), ref _icon, value); }
+        }
+
+        #endregion
+    }
+}
---
+++ SmsBuddy/SmsBuddy/ViewModels/MainViewModel.cs
@@ -0,0 +1,82 @@
+﻿using NullVoidCreations.WpfHelpers.Base;
+using NullVoidCreations.WpfHelpers.Commands;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Windows;
+using System.Windows.Input;
+
+namespace SmsBuddy.ViewModels
+{
+    class MainViewModel: ViewModelBase
+    {
+        Dictionary<ChildViewModelBase, FrameworkElement> _children;
+        FrameworkElement _child;
+        ICommand _initialize, _showChild;
+
+        #region properties
+
+        public IEnumerable<ChildViewModelBase> Children
+        {
+            get { return _children.Keys; }
+        }
+
+        public FrameworkElement Child
+        {
+            get { return _child; }
+            private set { Set(nameof(Child), ref _child, value); }
+        }
+
+        #endregion
+
+        #region commands
+
+        public ICommand InitializeCommand
+        {
+            get
+            {
+                if (_initialize == null)
+                    _initialize = new RelayCommand(Initialize);
+
+                return _initialize;
+            }
+        }
+
+        public ICommand ShowChildCommand
+        {
+            get
+            {
+                if (_showChild == null)
+                    _showChild = new RelayCommand<ChildViewModelBase>(ShowChild) { IsSynchronous = true };
+
+                return _showChild;
+            }
+        }
+
+        #endregion
+
+        void ShowChild(ChildViewModelBase childVm)
+        {
+
+        }
+
+        void Initialize()
+        {
+            var children = new Dictionary<ChildViewModelBase, FrameworkElement>();
+            var vmBaseType = typeof(ChildViewModelBase);
+            foreach(Type type in Assembly.GetExecutingAssembly().GetTypes())
+            {
+                if (type.IsSubclassOf(vmBaseType))
+                {
+                    var childVm = Activator.CreateInstance(type) as ChildViewModelBase;
+                    if (childVm == null)
+                        continue;
+
+                    children.Add(childVm, null);
+                }
+            }
+            _children = children;
+            RaisePropertyChanged(nameof(Children));
+        }
+    }
+}
