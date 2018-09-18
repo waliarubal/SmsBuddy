38eefa609da72a138e693ae45d920351f03864f5
Rubal Walia <rubal.walia@dotsquares.com>
2018-09-18 16:29:48 +0530

    Staging

---
+++ SmsBuddy/SmsBuddy/ViewModels/MessageViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace SmsBuddy.ViewModels
+{
+    class MessageViewModel: ChildViewModelBase
+    {
+        public MessageViewModel(): base("Message")
+        {
+            Icon = "/Assets/Images/sms-32.png";
+        }
+    }
+}
