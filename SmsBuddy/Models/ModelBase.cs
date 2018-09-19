32287eab4a1b7297db85765ba1fe828771e9b71f
Rubal Walia <rubal.walia@dotsquares.com>
2018-09-19 18:37:33 +0530

    Staging

---
+++ SmsBuddy/Models/ModelBase.cs
@@ -0,0 +1,19 @@
+﻿using NullVoidCreations.WpfHelpers.Base;
+
+namespace SmsBuddy.Models
+{
+    abstract class ModelBase: NotificationBase
+    {
+        long _id;
+
+        #region properties
+
+        public long Id
+        {
+            get { return _id; }
+            set { Set(nameof(Id), ref _id, value); }
+        }
+
+        #endregion
+    }
+}
---
+++ SmsBuddy/Models/TemplateModel.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.ObjectModel;
+
+namespace SmsBuddy.Models
+{
+    class TemplateModel: ModelBase
+    {
+        string _name, _template;
+        ObservableCollection<string> _fields;
+
+        public TemplateModel()
+        {
+            _fields = new ObservableCollection<string>();
+        }
+
+        #region properties
+
+        public string Name
+        {
+            get { return _name; }
+            set { Set(nameof(Name), ref _name, value); }
+        }
+
+        public ObservableCollection<string> Fields
+        {
+            get { return _fields; }
+            set { Set(nameof(Fields), ref _fields, value); }
+        }
+
+        public string Template
+        {
+            get { return _template; }
+            set { Set(nameof(Template), ref _template, value); }
+        }
+
+        #endregion
+    }
+}
