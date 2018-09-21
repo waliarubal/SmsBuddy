84d97ac63a1a72bf3b741afeb70a35d508260157
Rubal Walia <rubal.walia@dotsquares.com>
2018-09-21 12:44:30 +0530

    Staging

--- SmsBuddy/ViewModels/TemplateViewModel.cs
+++ SmsBuddy/ViewModels/TemplateViewModel.cs
@@ -1,6 +1,8 @@
 ﻿using NullVoidCreations.WpfHelpers.Base;
+using NullVoidCreations.WpfHelpers.Commands;
 using SmsBuddy.Models;
 using System.Collections.ObjectModel;
+using System.Windows.Input;
 
 namespace SmsBuddy.ViewModels
 {
@@ -8,6 +10,8 @@ namespace SmsBuddy.ViewModels
     {
         TemplateModel _template;
         ObservableCollection<TemplateModel> _templates;
+        string _newField, _selectedField;
+        ICommand _addField, _removeField, _addFieldToMessage;
 
         #region properties
 
@@ -23,6 +27,81 @@ namespace SmsBuddy.ViewModels
             set { Set(nameof(Templates), ref _templates, value); }
         }
 
+        public string NewField
+        {
+            get { return _newField; }
+            set { Set(nameof(NewField), ref _newField, value); }
+        }
+
+        public string SelectedField
+        {
+            get { return _selectedField; }
+            set { Set(nameof(SelectedField), ref _selectedField, value); }
+        }
+
         #endregion
+
+        #region commands
+
+        public ICommand AddFieldCommand
+        {
+            get
+            {
+                if (_addField == null)
+                    _addField = new RelayCommand(AddField) { IsSynchronous = true };
+
+                return _addField;
+            }
+        }
+
+        public ICommand RemoveFieldCommand
+        {
+            get
+            {
+                if (_removeField == null)
+                    _removeField = new RelayCommand(RemoveField) { IsSynchronous = true };
+
+                return _removeField;
+            }
+        }
+
+        public ICommand AddFieldToMessageCommand
+        {
+            get
+            {
+                if (_addFieldToMessage == null)
+                    _addFieldToMessage = new RelayCommand(AddFieldToMessage);
+
+                return _addFieldToMessage;
+            }
+        }
+
+        #endregion
+
+        void AddField()
+        {
+            if (Template == null || Template.Fields.Contains(NewField))
+                return;
+
+            Template.Fields.Contains(NewField);
+            NewField = null;
+        }
+
+        void RemoveField()
+        {
+            if (Template == null || !Template.Fields.Contains(SelectedField))
+                return;
+
+            Template.Fields.Remove(SelectedField);
+            SelectedField = null;
+        }
+
+        void AddFieldToMessage()
+        {
+            if (Template == null || string.IsNullOrWhiteSpace(SelectedField))
+                return;
+
+            Template.Message = string.Format("{0}<<{1}>>", Template.Message, SelectedField);
+        }
     }
 }
