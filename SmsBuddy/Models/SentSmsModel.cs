bfb683bb26a4a7fa79ba860cf56b90bb5b47710b
Rubal Walia <walia.rubal@gmail.com>
2018-09-25 01:48:27 +0530

    Staging

--- SmsBuddy/Models/SentSmsModel.cs
+++ SmsBuddy/Models/SentSmsModel.cs
@@ -1,8 +1,68 @@
-﻿using NullVoidCreations.WpfHelpers.Base;
+﻿using LiteDB;
+using NullVoidCreations.WpfHelpers.Base;
+using System;
+using System.Collections.Generic;
 
 namespace SmsBuddy.Models
 {
-    class SentSmsModel: NotificationBase
+    class SentSmsModel: NotificationBase, IModel
     {
+        long _id;
+        string _mobileNumber, _message;
+        DateTime _time;
+
+        #region properties
+
+        [BsonId(true)]
+        public long Id
+        {
+            get { return _id; }
+            set { Set(nameof(Id), ref _id, value); }
+        }
+
+        public string MobileNumber
+        {
+            get { return _mobileNumber; }
+            set { Set(nameof(MobileNumber), ref _mobileNumber, value); }
+        }
+
+        public string Message
+        {
+            get { return _message; }
+            set { Set(nameof(Message), ref _message, value); }
+        }
+
+        public DateTime Time
+        {
+            get { return _time; }
+            set { Set(nameof(Time), ref _time, value); }
+        }
+
+        #endregion
+
+        public bool Delete()
+        {
+            var db = Shared.Instance.Database;
+            var collection = db.GetCollection<SentSmsModel>();
+            return collection.Delete(Id);
+        }
+
+        public IEnumerable<IModel> Get()
+        {
+            var db = Shared.Instance.Database;
+            return db.GetCollection<SentSmsModel>().FindAll();
+        }
+
+        public bool Save()
+        {
+            var db = Shared.Instance.Database;
+            var collection = db.GetCollection<SentSmsModel>();
+            var isSaved = collection.Update(this);
+            if (isSaved)
+                return isSaved;
+
+            Id = collection.Insert(this);
+            return true;
+        }
     }
 }
--- SmsBuddy/ViewModels/SentSmsViewModel.cs
+++ SmsBuddy/ViewModels/SentSmsViewModel.cs
@@ -1,7 +1,46 @@
-﻿namespace SmsBuddy.ViewModels
+﻿using NullVoidCreations.WpfHelpers.Commands;
+using SmsBuddy.Models;
+using System.Collections.Generic;
+using System.Windows.Input;
+
+namespace SmsBuddy.ViewModels
 {
     class SentSmsViewModel: ChildViewModelBase
     {
+        IEnumerable<SentSmsModel> _messages;
+        ICommand _refresh;
+
         public SentSmsViewModel(): base("Outbox", "outbox-32.png") { }
+
+        #region properties
+
+        public IEnumerable<SentSmsModel> Messages
+        {
+            get { return _messages; }
+            private set { Set(nameof(Messages), ref _messages, value); }
+        }
+
+        #endregion
+
+        #region commands
+
+        public ICommand RefreshCommand
+        {
+            get
+            {
+                if (_refresh == null)
+                    _refresh = new RelayCommand(Refresh);
+
+                return _refresh;
+            }
+        }
+
+        #endregion
+
+        void Refresh()
+        {
+            ErrorMessage = null;
+            Messages = new SentSmsModel().Get() as IEnumerable<SentSmsModel>;
+        }
     }
 }
